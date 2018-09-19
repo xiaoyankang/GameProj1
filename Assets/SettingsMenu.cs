c9c9cc0872e2bff38c2445cac3eb8205805f0b33
Xiaoyan Kang <kang_xiaoyan@berkeley.edu>
2018-09-18 18:46:46 -0700

    proj1-2

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+    public GameObject canvas;
+
+    public void Activate() {
+        canvas.SetActive(true);
+    }
+
+    public void Back() {
+        canvas.SetActive(false);
+    }
+
+    public void SetVolume (float volume) {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+}
